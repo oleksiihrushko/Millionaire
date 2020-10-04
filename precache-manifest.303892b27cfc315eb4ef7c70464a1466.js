3576c27ffc9909b2b936d177ea47c24b2d748896
hrushkooleksii <oleksiihrushko@gmail.com>
2020-10-04 15:15:52 +0300

    Updates

---
+++ precache-manifest.303892b27cfc315eb4ef7c70464a1466.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3f29d5bb0722602265de8ce07ba15f4",
+    "url": "/millionaire/index.html"
+  },
+  {
+    "revision": "4050b40f0abea0938785",
+    "url": "/millionaire/static/css/main.5300d95b.chunk.css"
+  },
+  {
+    "revision": "db203f721ff701cb0e62",
+    "url": "/millionaire/static/js/2.dca1fdfb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/millionaire/static/js/2.dca1fdfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4050b40f0abea0938785",
+    "url": "/millionaire/static/js/main.2cfa553c.chunk.js"
+  },
+  {
+    "revision": "078e70291101148e5f5b",
+    "url": "/millionaire/static/js/runtime-main.70d563fc.js"
+  },
+  {
+    "revision": "f8c75481696354bfdabed0ba328d3476",
+    "url": "/millionaire/static/media/hand.f8c75481.png"
+  }
+]);
