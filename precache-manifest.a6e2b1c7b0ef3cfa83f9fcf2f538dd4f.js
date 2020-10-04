b1f02d190849a6459e5930a12978bc32980083f1
hrushkooleksii <oleksiihrushko@gmail.com>
2020-10-04 15:01:56 +0300

    Updates

---
+++ precache-manifest.a6e2b1c7b0ef3cfa83f9fcf2f538dd4f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb8521e475c86e322cc9550554d360a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4050b40f0abea0938785",
+    "url": "/static/css/main.5300d95b.chunk.css"
+  },
+  {
+    "revision": "db203f721ff701cb0e62",
+    "url": "/static/js/2.dca1fdfb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.dca1fdfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4050b40f0abea0938785",
+    "url": "/static/js/main.2cfa553c.chunk.js"
+  },
+  {
+    "revision": "7763e772531280d699d6",
+    "url": "/static/js/runtime-main.cdfacf38.js"
+  },
+  {
+    "revision": "f8c75481696354bfdabed0ba328d3476",
+    "url": "/static/media/hand.f8c75481.png"
+  }
+]);
